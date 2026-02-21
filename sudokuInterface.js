c57756c1a3008a1689c1f5f97a00d06681aaac93
Neonicc <n.s.watson95@gmail.com>
2026-02-20 21:54:20 -0600

    fixed cell staying queued after newgame selection

--- sudokuInterface.js
+++ sudokuInterface.js
@@ -151,6 +151,7 @@ let myGame = new SudokuGame();
 myGame.generateBoard(0,0);
 
 function setGame() {
+    queuedCell = null;
     gameTitle.style.display = 'none';
     leaderboardContainer.style.display = 'none';
     scoreBar.style.display = 'flex';
